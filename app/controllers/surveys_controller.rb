c72be40b78ce322336e14d7255c026f942f606e8
deveshdedhia <deveshdedhia@gmail.com>
2015-04-29 16:40:00 -0400

    Edit Survey Functionality Added

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -6,14 +6,7 @@ class SurveysController < ApplicationController
 
 	def create
 		@survey = Survey.new(survey_params)
-		@survey.questions.each do |q|
-			if q.choice_list.to_s != ''
-				choice_options = q.choice_list.split(',')
-				choice_options.each do |choice_text|
-					q.choices.build(choice_text: choice_text.strip)
-				end
-			end
-		end
+		choice_list_to_choices
 		if @survey.save
 			redirect_to surveys_path
 		else
@@ -22,6 +15,21 @@ class SurveysController < ApplicationController
 		end
 	end
 
+	def edit
+		@survey = Survey.find(params[:id])
+		@question_types = [['Textbox', 'textbox'], ['Checkbox', 'checkbox'],['Dropdown','dropdown']]
+	end
+
+	def update
+		@survey = Survey.find(params[:id])
+		if @survey != nil
+			if(@survey.update(survey_params))
+				redirect_to surveys_path
+			else
+				redirect_to edit_survey_path(@survey)
+			end
+		end
+	end
 
 	def new
 		@survey = Survey.new
@@ -32,5 +40,16 @@ class SurveysController < ApplicationController
 	private
 	def survey_params
 		params.require(:survey).permit!
+	end
+
+	def choice_list_to_choices 
+		@survey.questions.each do |q|
+			if q.choice_list.to_s != ''
+				choice_options = q.choice_list.split(',')
+				choice_options.each do |choice_text|
+					q.choices.build(choice_text: choice_text.strip)
+				end
+			end
+		end
 	end
 end
